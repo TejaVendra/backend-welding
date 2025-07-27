a1b47897fec9f726d1f52115e3efcfc26379af65
Teja <tejavitap@gmail.com>
2025-07-27 20:29:48 +0530

    Prepare for Render deployment

--- backendServer/settings.py
+++ backendServer/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'backendServer.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
 # Password validation
